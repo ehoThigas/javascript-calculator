b3e5fab145dba3d4a81fbe4c2f82ab92c70a1bdd
Thiago Lucas <thiago.lucasc@outlook.com>
2026-01-23 11:03:57 -0300

    Refined the input functions, added the calculate function, fixed some bugs

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.querySelectorAll('button').forEach(button => {
         };
 
         if(this.id == 'result'){
-            clear();
+            calculate();
         };
     });
 });
@@ -34,10 +34,27 @@ function clear(){
 };
 
 function addContent(content){
+    if(isErrorState()){
+        clear();
+    };
+
     output.value += content;
 };
 
 function addOperator(content){
+    if(isErrorState()){
+        clear();
+    };
+
+    const expression = output.value.trim();
+    const lastChar = expression.slice(-1);
+
+    if(!expression && content !== '-') return;
+
+    if(['+', '\u00F7', 'x', ' ', '.'].includes(lastChar) && content !== '-') return;
+
+    if(['-', ' ', '.'].includes(lastChar)) return;
+
     switch(content){
         case "%":
             output.value += "\u00F7";
@@ -52,6 +69,10 @@ function addOperator(content){
 };
 
 function addDecimal(){
+    if(isErrorState()){
+        clear();
+    };
+
     const expression = output.value.trim();
     const lastChar = expression.slice(-1);
     const lastOperator = Math.max(
@@ -72,11 +93,44 @@ function addDecimal(){
         return;
     };
 
-    if(currentNumber.includes('.')){
-        return;
-    }
+    if(currentNumber.includes('.')) return;
 
     output.value += '.';
-}
+};
 
+function calculate(){
+    if(!endsWithNumber(output.value)){
+        output.value = "[ERRO] Expressão Inválida!"
+        return;
+    };
+
+    try {
+        const normalized = normalizeExpression(output.value);
+        const result = Function(`"use strict"; return (${normalized})`)();
+
+        if(!Number.isFinite(result)){
+            throw new Error("Invalid math");
+        }
+
+        output.value = result;
+    } catch {
+        output.value = "[ERRO] Expressão Inválida!";
+    }
+};
+
+function endsWithNumber(value){
+    return /\d$/.test(value);
+};
+
+function isErrorState(){
+    if(output.value.includes("[ERRO]")){
+        return true;
+    };
+};
+
+function normalizeExpression(exp){
+    return exp
+        .replace(/x/g, '*')
+        .replace(/÷/g, '/');
+};
 });
